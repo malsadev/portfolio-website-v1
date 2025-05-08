5a940c6d6fd9ed306c036d23cab2add81cf12cf4
Mustafa Al-Sakkaf <malsa080@uottawa.ca>
2025-05-08 16:04:47 -0400

    fixed: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 #https://learnvue.co/articles/deploy-vue-to-github-pages
+git branch -d <branch_name>
+
+npm install
+
 npm run build
+
+git add . && git commit -m "build"
 
 git add dist && git commit -m "adding dist subtree"
 
